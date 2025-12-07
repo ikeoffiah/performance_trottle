30f20f73cee8635223d6f1bd73856001ebf5d9c1
Pius-CL <piuspio12@gmail.com>
2025-12-07 11:53:21 +0000

    update: made some optimal change in multline_chart

--- lib/views/custom_widget/multiline_chart.dart
+++ lib/views/custom_widget/multiline_chart.dart
@@ -8,60 +8,62 @@ class MultilineChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Selector<AccelViewModel, double?>(
-      selector: (_, vm) => vm.mag.isEmpty ? null : vm.mag.last,
-      builder: (context, latestMag, __) {
-        final vm = context.read<AccelViewModel>();
-        final len = vm.mag.length;
-        final spotsX = List.generate(len, (i) => FlSpot(i.toDouble(), vm.x[i]));
-        final spotsY = List.generate(len, (i) => FlSpot(i.toDouble(), vm.y[i]));
-        final spotsZ = List.generate(len, (i) => FlSpot(i.toDouble(), vm.z[i]));
-        final spotsM = List.generate(len, (i) => FlSpot(i.toDouble(), vm.mag[i]));
+    final vm = context.watch<AccelViewModel>();
+    final len = vm.mag.length;
+    
+    if (len == 0) {
+      return const Center(
+        child: Text('No data yet', style: TextStyle(color: Colors.grey)),
+      );
+    }
 
-        return LineChart(
-          LineChartData(
-            gridData: FlGridData(
-              show: true,
-              horizontalInterval: 2,
-              verticalInterval: 20,
-            ),
-            titlesData: FlTitlesData(show: false),
-            borderData: FlBorderData(show: false),
-            minY: -20,
-            maxY: 20,
-            lineBarsData: <LineChartBarData>[
-              LineChartBarData(
-                spots: spotsX,
-                isCurved: false,
-                dotData: FlDotData(show: false),
-                barWidth: 2,
-                color: Colors.red,
-              ),
-              LineChartBarData(
-                spots: spotsY,
-                isCurved: false,
-                dotData: FlDotData(show: false),
-                barWidth: 2,
-                color: Colors.green,
-              ),
-              LineChartBarData(
-                spots: spotsZ,
-                isCurved: false,
-                dotData: FlDotData(show: false),
-                barWidth: 2,
-                color: Colors.blue,
-              ),
-              LineChartBarData(
-                spots: spotsM,
-                isCurved: true,
-                dotData: FlDotData(show: false),
-                barWidth: 3,
-                color: Colors.purple,
-              ),
-            ],
+    final spotsX = List.generate(len, (i) => FlSpot(i.toDouble(), vm.x[i]));
+    final spotsY = List.generate(len, (i) => FlSpot(i.toDouble(), vm.y[i]));
+    final spotsZ = List.generate(len, (i) => FlSpot(i.toDouble(), vm.z[i]));
+    final spotsM = List.generate(len, (i) => FlSpot(i.toDouble(), vm.mag[i]));
+
+    return LineChart(
+      LineChartData(
+        gridData: const FlGridData(
+          show: true,
+          horizontalInterval: 2,
+          verticalInterval: 20,
+        ),
+        titlesData: const FlTitlesData(show: false),
+        borderData: FlBorderData(show: false),
+        minY: -20,
+        maxY: 20,
+        lineBarsData: <LineChartBarData>[
+          LineChartBarData(
+            spots: spotsX,
+            isCurved: false,
+            dotData: const FlDotData(show: false),
+            barWidth: 2,
+            color: Colors.red,
           ),
-        );
-      },
+          LineChartBarData(
+            spots: spotsY,
+            isCurved: false,
+            dotData: const FlDotData(show: false),
+            barWidth: 2,
+            color: Colors.green,
+          ),
+          LineChartBarData(
+            spots: spotsZ,
+            isCurved: false,
+            dotData: const FlDotData(show: false),
+            barWidth: 2,
+            color: Colors.blue,
+          ),
+          LineChartBarData(
+            spots: spotsM,
+            isCurved: true,
+            dotData: const FlDotData(show: false),
+            barWidth: 3,
+            color: Colors.purple,
+          ),
+        ],
+      ),
     );
   }
 }
